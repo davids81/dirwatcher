2fa7c97513b6ba0914f796c3f0582e21e644b611
Dave Stockdale <DStockdale@wagstaff.com>
2019-02-07 09:52:28 -0800

    updated logging

--- job.py
+++ job.py
@@ -2,7 +2,9 @@
 import os
 from os import listdir
 from os.path import isfile, join
+from datetime import datetime
 import subprocess
+
 
 class job:
     def __init__(self, dir, cmd, params):
@@ -16,6 +18,6 @@ class job:
             cmdText = cmdText + ' ' + p['option'] + ' '
         cmdText = cmdText + "\"" + file + "\""
         FNULL = open(os.devnull, 'w')
-        print('Executing: ' + cmdText)
+        print('Executing: ' + cmdText + " at: " + datetime.now().strftime('%H:%M:%S'))
         print("")
-        #subprocess.call(cmdText, stdout=FNULL, stderr=FNULL, shell=False)
+        subprocess.call(cmdText, stdout=FNULL, stderr=FNULL, shell=False)
